1902043de766e33188e565eacb0f0ba87670fda9
Tu TV <tutv95@gmail.com>
2015-10-20 01:56:53 +0700

    Updated v1 api

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -11,22 +11,23 @@
 |
 */
 
+Route::group( array( 'prefix' => 'v1' ), function () {
+	Route::group( array( 'prefix' => 'location' ), function () {
+		// Get location by coordinates
+		Route::get( 'byCoordinates/lat={lat}&lng={lng}',
+			'LocationController@byCoordinates' );
 
-Route::group( array( 'prefix' => 'location' ), function () {
-	// Get location by coordinates
-	Route::get( 'byCoordinates/lat={lat}&lng={lng}',
-		'LocationController@byCoordinates' );
+		//Get location by place_id
+		Route::get( 'byPlaceID/id={id}', 'LocationController@byPlaceID' );
+	} );
 
-	//Get location by place_id
-	Route::get( 'byPlaceID/id={id}', 'LocationController@byPlaceID' );
-} );
+	Route::group( array( 'prefix' => 'direction' ), function () {
+		// Get direction by Text query
+		Route::get( 'byText/origin={origin}&destination={destination}',
+			'DirectionController@byText' );
 
-Route::group( array( 'prefix' => 'direction' ), function () {
-	// Get direction by Text query
-	Route::get( 'byText/origin={origin}&destination={destination}',
-		'DirectionController@byText' );
-
-	// Get direction by place_id
-	Route::get( 'byPlaceID/origin={origin}&destination={destination}',
-		'DirectionController@byPlaceID' );
+		// Get direction by place_id
+		Route::get( 'byPlaceID/origin={origin}&destination={destination}',
+			'DirectionController@byPlaceID' );
+	} );
 } );
